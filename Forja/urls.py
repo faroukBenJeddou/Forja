b6bc3999cce097489e9a42cdb84dc80301f0452e
MalekZaidi <melekzaidi407@gmail.com>
2024-09-29 10:55:33 +0100

    Add template

--- Forja/urls.py
+++ Forja/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ForjaApp import views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+urlpatterns = [path('', views.index, name='index'),
+    path('admin/', admin.site.urls), 
 ]
